fe6db11786b9c7305266d0f1eb0ffab724b5503b
Juan Esteban <valdezjuanes014@gmail.com>
2024-02-09 19:00:21 -0500

    Delete for package in spanish and modified Repository

--- src/main/java/com/project/coches/domain/repository/ICarBrandRepository.java
+++ src/main/java/com/project/coches/domain/repository/ICarBrandRepository.java
@@ -3,10 +3,38 @@ package com.project.coches.domain.repository;
 import com.project.coches.domain.pojo.CarBrandPojo;
 
 import java.util.List;
+import java.util.Optional;
 
+/**
+ * Interfaz de marca coche
+ */
 public interface ICarBrandRepository {
 
+    /**
+     * Obtiene lista de marcas coches pojo
+     * @return lista de MarcasCochesPojo
+     */
     List<CarBrandPojo> getAll();
 
+    /**
+     * Optional (en caso de encontrar devuelve la MarcaCoche y
+     * en caso de que no, evita el null)
+     * @param id Marca de coche a buscar
+     * @return Marca de coche encontrada
+     */
+    Optional<CarBrandPojo> getCarBrand(Integer id);
+
+    /**
+     * Guarda una marca de coche
+     * @param carBrandPojoNew MarcaCochePojo nueva
+     * @return Guarda una MarcaCochePojo
+     */
+    CarBrandPojo save(CarBrandPojo carBrandPojoNew);
+
+    /**
+     * Elimina una marca de coche
+     * @param idCarBrand Marca de coche a eliminar
+     */
+    void delete(Integer idCarBrand);
 
 }
--- src/main/java/com/project/coches/persistance/repository/CarBrandRepository.java
+++ src/main/java/com/project/coches/persistance/repository/CarBrandRepository.java
@@ -1,7 +1,54 @@
 package com.project.coches.persistance.repository;
 
+import com.project.coches.domain.pojo.CarBrandPojo;
+import com.project.coches.domain.repository.ICarBrandRepository;
+import com.project.coches.persistance.entity.CarBrandEntity;
+import com.project.coches.persistance.mapper.ICarBrandMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
 @Repository
-public class CarBrandRepository {
+public class CarBrandRepository implements ICarBrandRepository {
+
+    private final ICarBrandCrudRepository iCarBrandCrudRepository;
+
+    private final ICarBrandMapper iCarBrandMapper;
+
+    /**
+     * Lista de MarcasCochesPojo a través de listar MarcasCochesEntity
+     * @return Lista de MarcasCochesPojo
+     */
+    @Override
+    public List<CarBrandPojo> getAll() {
+        return iCarBrandMapper.toMarcasCochePojo(iCarBrandCrudRepository.findAll());
+    }
+
+    @Override
+    public Optional<CarBrandPojo> getCarBrand(Integer id) {
+        try {
+
+        } catch (Exception e){
+
+        }
+    }
+
+    /**
+     * Guardar una MarcaCochePojo y para la BD una MarcaCocheEntity
+     * @param carBrandPojoNew MarcaCochePojo nueva
+     * @return MarcaCochePojo guardada
+     */
+    @Override
+    public CarBrandPojo save(CarBrandPojo carBrandPojoNew) {
+        CarBrandEntity carBrandEntity = iCarBrandMapper.toMarcaCocheEntity(carBrandPojoNew);
+        return iCarBrandMapper.toMarcaCochePojo(iCarBrandCrudRepository.save(carBrandEntity));
+    }
+
+    @Override
+    public void delete(Integer idCarBrand) {
+
+    }
 }
